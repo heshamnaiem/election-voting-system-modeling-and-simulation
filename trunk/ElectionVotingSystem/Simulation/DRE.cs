f69d6634a51c489c6cc387db01a9db6b2ce5c150
mostafa.3ez@gmail.com <mostafa.3ez@gmail.com@5440b410-71d6-4002-b11a-37d0787ee3f9>
2011-05-12 11:03:56 +0000

    Starting of DRE class implementation which is responsible for the task of the voting (generate delay of gamma distribution )

--- trunk/ElectionVotingSystem/Simulation/DRE.cs
+++ trunk/ElectionVotingSystem/Simulation/DRE.cs
@@ -3,10 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using React;
+using React.Distribution;
 
 namespace ElectionVotingSystem.Simulation
 {
-    class DRE   //direct-recording electronic machine
+    class DRE : Process  //direct-recording electronic machine
     {
+        int capacity;
+        public int Capacity
+        {
+            set
+            {
+                capacity = value;
+            }
+
+            get
+            {
+                return capacity;
+            }
+        }
+        int ID;
+        public int MachineID
+        {
+            set 
+            {
+                ID = value;
+            }
+            get
+            {
+                return ID;
+            }
+        }
+        DRE(React.Simulation sim)
+            : base(sim)
+        {        }
+
+        // we need more investigation on this
+        // Gamma scale parameter = 0.58 ~ 1.05 
+        // Gamma Shape parameter = 5.71
+        Gamma gamma = new Gamma(0.58, 5.71);
+
+        protected override IEnumerator<Task> GetProcessSteps()
+        {
+            // get delay in Milliseconds
+            int delay = (int)(gamma.NextSingle() * 60 * 1000);
+            // starting delay
+            yield return Delay(delay); 
+            // end of delay
+            yield break;
+        }
+        
     }
 }
