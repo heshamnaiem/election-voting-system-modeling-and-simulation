27714d98337345e53d88ef4a04f63eb6108c4735
em.cmp.eng@gmail.com <em.cmp.eng@gmail.com@5440b410-71d6-4002-b11a-37d0787ee3f9>
2011-05-13 19:34:41 +0000

    Starting of The Voter class implementation

--- ElectionVotingSystem/Simulation/Voter.cs
+++ ElectionVotingSystem/Simulation/Voter.cs
@@ -6,7 +6,28 @@ using React;
 
 namespace ElectionVotingSystem.Simulation
 {
-    class Voter
-    {
+    class Voter : Process
+    { 
+        // turnout rate - Normal - determin the turnout - control creating the voters for each Precinct
+        // arrival rate - Poisson distribution 
+        internal Voter(React.Simulation sim)
+            : base(sim)
+        {
+        }
+
+        protected override IEnumerator<Task> GetProcessSteps()
+        {
+            Resource DREs = (Resource)ActivationData;
+            yield return DREs.Acquire(this);
+
+            System.Diagnostics.Debug.Assert(DREs == Activator);
+            System.Diagnostics.Debug.Assert(ActivationData != null);
+            DRE dre = (DRE)ActivationData;
+
+            yield return dre;
+
+            yield return DREs.Release(this);
+        }
+
     }
 }
