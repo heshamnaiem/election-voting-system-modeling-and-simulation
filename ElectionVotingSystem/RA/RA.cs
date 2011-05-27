55b2d02c1244b9e41c873dde1ce1504483441256
em.cmp.eng@gmail.com <em.cmp.eng@gmail.com@5440b410-71d6-4002-b11a-37d0787ee3f9>
2011-05-27 14:03:43 +0000

    Make RandomAlgo choose the Best Combination between 3 Combinations "changeable"

--- ElectionVotingSystem/RA/RA.cs
+++ ElectionVotingSystem/RA/RA.cs
@@ -11,6 +11,9 @@ namespace ElectionVotingSystem
         int Precinct_No;        //Number Of Precincts
         int DRE_No;        //Number Of DRE Machines
 
+        int[,] save;
+        public int[] Best;
+
         Precinct[] prec;
         Task[] t;
 
@@ -19,24 +22,56 @@ namespace ElectionVotingSystem
             this.Precinct_No = precinct_no;
             this.DRE_No = dre_no;
 
+            int[,] save = new int[3, Precinct_No + 1];
+            int[] Best = new int[Precinct_No];
+
             this.prec = new Precinct[precinct_no];
             this.t = new Task[precinct_no];
         }
 
         public void RandomAlgo()
         {
-            Random rand = new Random();
+            long temp = 2147483647;
 
-
-            for (int i = 0; i < this.Precinct_No; i++)
+            for (int j = 0; j < 3; j++)
             {
-                this.prec[i] = new Precinct(i + 1);
-                this.prec[i].Xi = rand.Next(1, this.DRE_No / 2);
-                this.t[i] = new Process(prec[0], prec[0].Generator, prec[i].GetPrecinctNumber());
+                Random rand = new Random();
+                Voter.MaxWaitingTime = -1;
+                Voter.PrecinctNumber = -1;
+                Voter.ResetM_W_T_P();
+
+                for (int i = 0; i < this.Precinct_No; i++)
+                {
+                    this.prec[i] = new Precinct(i + 1);
+                    this.prec[i].Xi = rand.Next(1, this.DRE_No / 2);
+                    save[j, prec[i].GetPrecinctNumber()] = prec[i].Xi;
+                    this.t[i] = new Process(prec[0], prec[0].Generator, prec[i].GetPrecinctNumber());
+                }
+
+                prec[0].Run(t);
+
+                save[j, 0] = (int)Voter.MaxWaitingTime;
             }
 
-            prec[0].Run(t);
+            for (int j = 0; j < 3; j++)
+            {
+                if (temp > save[j, 0])
+                {
+                    temp = save[j, 0];
+                }
+            }
+            for (int j = 0; j < 3; j++)
+            {
+                if (temp == save[j, 0])
+                {
+                    for (int i = 0; i < Precinct_No; i++)
+                    {
+                        Best[i] = save[j, i + 1];
+                    }
+                }
+            }
+
         }
-    
+
     }
 }
